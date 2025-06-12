50484392b226f97a4a4f5b884dc877dbb0864eb0
ImLostPlsHelp <maurits1005@gmail.com>
2025-06-12 16:55:43 +0700

    Fixing port, host, and cors in server

--- backend/routes.js
+++ backend/routes.js
@@ -12,66 +12,30 @@ export const routes = [
     method: "POST",
     path: "/api/signup",
     handler: SignUpHandler,
-    options: {
-      cors: {
-        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
-        credentials: true,
-      },
-    },
   },
   {
     method: "POST",
     path: "/api/login",
     handler: LoginHandler,
-    options: {
-      cors: {
-        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
-        credentials: true,
-      },
-    },
   },
   {
     method: "POST",
     path: "/api/get-groq-advice",
     handler: GroqHandler,
-    options: {
-      cors: {
-        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
-        credentials: true,
-      },
-    },
   },
   {
     method: "POST",
     path: "/api/save-result",
     handler: saveResultHandler,
-    options: {
-      cors: {
-        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
-        credentials: true,
-      },
-    },
   },
   {
     method: "GET",
     path: "/api/get-result",
     handler: getScanHistoryHandler,
-    options: {
-      cors: {
-        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
-        credentials: true,
-      },
-    },
   },
   {
     method: "GET",
     path: "/api/get-profile",
     handler: getUserInformation,
-    options: {
-      cors: {
-        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
-        credentials: true,
-      },
-    },
   },
 ];
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,35 @@
-import Hapi from '@hapi/hapi';
-import dotenv from 'dotenv';
-import { routes } from './routes.js';
+import Hapi from "@hapi/hapi";
+import dotenv from "dotenv";
+import { routes } from "./routes.js";
 dotenv.config();
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT || 3000,
+    host: "0.0.0.0",
     routes: {
-    cors: {
-      origin: ['https://capstone-dbs-skinalyze.vercel.app'],
-      credentials: true,
+      cors: {
+        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
+        credentials: true,
+        headers: [
+          "Accept",
+          "Authorization",
+          "Content-Type",
+          "If-None-Match",
+          "Origin",
+          "X-Requested-With",
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+        exposedHeaders: ["WWW-Authenticate", "Server-Authorization"],
+        maxAge: 600,
+      },
     },
-  },
-});
+  });
 
   server.route(routes);
 
   await server.start();
-  console.log('Server running on', server.info.uri);
+  console.log("Server running on", server.info.uri);
 };
 
 init();
