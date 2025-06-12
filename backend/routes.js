964e047e944bb71a21d3804799d62d617dfaca3f
ImLostPlsHelp <maurits1005@gmail.com>
2025-06-12 16:46:17 +0700

    Adding more cors

--- backend/routes.js
+++ backend/routes.js
@@ -1,40 +1,77 @@
-import {SignUpHandler, LoginHandler, GroqHandler, saveResultHandler, getScanHistoryHandler, getUserInformation} from './handler.js';
+import {
+  SignUpHandler,
+  LoginHandler,
+  GroqHandler,
+  saveResultHandler,
+  getScanHistoryHandler,
+  getUserInformation,
+} from "./handler.js";
 
 export const routes = [
-    {
-        method: 'POST',
-        path: '/api/signup',
-        handler: SignUpHandler,
-        options: {
-            cors: {
-                origin: ['https://capstone-dbs-skinalyze.vercel.app'],
-                credentials: true,
-            }
-        }
+  {
+    method: "POST",
+    path: "/api/signup",
+    handler: SignUpHandler,
+    options: {
+      cors: {
+        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
+        credentials: true,
+      },
     },
-    {
-        method: 'POST',
-        path: '/api/login',
-        handler: LoginHandler,
+  },
+  {
+    method: "POST",
+    path: "/api/login",
+    handler: LoginHandler,
+    options: {
+      cors: {
+        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
+        credentials: true,
+      },
     },
-    {
-        method: 'POST',
-        path: '/api/get-groq-advice',
-        handler: GroqHandler,
+  },
+  {
+    method: "POST",
+    path: "/api/get-groq-advice",
+    handler: GroqHandler,
+    options: {
+      cors: {
+        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
+        credentials: true,
+      },
     },
-    {
-        method: 'POST',
-        path: '/api/save-result',
-        handler: saveResultHandler,
+  },
+  {
+    method: "POST",
+    path: "/api/save-result",
+    handler: saveResultHandler,
+    options: {
+      cors: {
+        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
+        credentials: true,
+      },
     },
-    {
-        method: 'GET',
-        path: '/api/get-result',
-        handler: getScanHistoryHandler,
+  },
+  {
+    method: "GET",
+    path: "/api/get-result",
+    handler: getScanHistoryHandler,
+    options: {
+      cors: {
+        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
+        credentials: true,
+      },
     },
-    {
-        method: 'GET',
-        path: '/api/get-profile',
-        handler: getUserInformation,
-    }
+  },
+  {
+    method: "GET",
+    path: "/api/get-profile",
+    handler: getUserInformation,
+    options: {
+      cors: {
+        origin: ["https://capstone-dbs-skinalyze.vercel.app"],
+        credentials: true,
+      },
+    },
+  },
 ];
